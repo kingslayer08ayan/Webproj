8ed289974f8c209eb9e95499bb9e654de0ee562f
unknown <ayanmaity874@gmail.com>
2023-03-07 09:38:01 +0530

    to count and restrict inputs

---
+++ RealTimeCharCounter/index.js
@@ -0,0 +1,11 @@
+const textareaEL=document.getElementById("textarea");
+const totalCounterEl=document.getElementById("total-counter");
+const remainingCounterEl=document.getElementById("remaining-counter");
+textareaEL.addEventListener("keyup",()=>{
+    updateCounter()
+});
+updateCounter()
+function updateCounter(){
+    totalCounterEl.innerText=textareaEL.value.length
+    remainingCounterEl.innerText=textareaEL.getAttribute("maxlength")-textareaEL.value.length
+}
